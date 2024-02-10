94f9358e1ce670f1acaa481b0f4cbd5c85f21b89
m.nechaev <m.nechaev@ritm.media>
2024-02-11 00:52:26 +0300

    Initial Commit

---
+++ 3Kick_tableview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  3Kick_tableview
+//
+//  Created by Нечаев Михаил on 11.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
